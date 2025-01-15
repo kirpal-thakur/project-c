260f3fa496b1f238a9f2b100fd6cc9dc518e5275
kavita <kavitakavita.cts@gmail.com>
2025-01-15 17:10:46 +0530

    issues-done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -46,7 +46,7 @@ export const environment = {
     pages:[
       'home','talent','clubs_and_scouts','about_us','pricing','news','contact','imprint','privacy_policy','terms_and_conditions','cookie_policy','faq','content_page'
     ],
-    apiUrl:'https://apitest.socceryou.ch/api/',
+    apiUrl:'https://api.socceryou.ch/',
     socketUrl:'https://alerts.socceryou.ch/',
     mailchimp: {
       apiUrl: 'https://us5.api.mailchimp.com/3.0/lists/7afbbb070a/members',
